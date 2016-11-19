7fb8e42a204c5490311a29e00331283c5adb62bb
Dmitri Koulikoff <dima@koulikoff.ru>
2016-11-19 09:47:20 +0100

    Gems added

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Bowling
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.sass.preferred_syntax = :sass
+    config.generators do |g|
+      g.cancan :current_ability
+    end
   end
 end
